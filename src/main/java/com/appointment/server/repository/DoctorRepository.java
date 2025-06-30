4f52e378fdfcbed509212767445a7770a7a6eddf
en-rypted <shivwackchaure19@gmail.com>
2025-07-01 00:17:22 +0530

    Added Repository for all newly created Collections Created apis for Doctor, Patient.

--- src/main/java/com/appointment/server/repository/DoctorRepository.java
+++ src/main/java/com/appointment/server/repository/DoctorRepository.java
@@ -1,12 +1,13 @@
 package com.appointment.server.repository;
 
 import com.appointment.server.entity.Doctor;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface DoctorRepository extends MongoRepository<Doctor,String> {
+public interface DoctorRepository extends MongoRepository<Doctor, ObjectId> {
     Optional<Doctor> findByEmail(String email);
 }
--- src/main/java/com/appointment/server/repository/PatientRepository.java
+++ src/main/java/com/appointment/server/repository/PatientRepository.java
@@ -1,14 +1,10 @@
 package com.appointment.server.repository;
 
-import com.appointment.server.entity.User;
+import com.appointment.server.entity.Patient;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface UserRepository extends MongoRepository<User, ObjectId> {
-
-    Optional<User> findByUsername(String username);
+public interface PatientRepository extends MongoRepository<Patient , ObjectId> {
 }
