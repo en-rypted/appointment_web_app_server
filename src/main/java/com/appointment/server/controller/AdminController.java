f92241f380dab8808e1e739cdb6bcea69ab5e198
en-rypted <shivwackchaure19@gmail.com>
2025-07-02 01:44:04 +0530

    Added AdminController.java and api for addSpeciality Added GlobalExceptionHandler to handle error response

--- src/main/java/com/appointment/server/controller/AdminController.java
+++ src/main/java/com/appointment/server/controller/AdminController.java
@@ -1,9 +1,26 @@
 package com.appointment.server.controller;
 
+import com.appointment.server.entity.Speciality;
+import com.appointment.server.service.AdminService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService){
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/addSpeciality")
+    public ResponseEntity<Object> addSpeciality(@Valid @RequestBody Speciality speciality){
+        return adminService.addSpeciality(speciality);
+    }
 }
